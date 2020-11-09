9e5b4ecc6bb420ce39951575d83bfafc13a5367e
Vu Nguyen <vu.nguyen101@gmail.com>
2020-11-09 17:00:13 +0200

    add fetchProducts fn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Product from './components/Product'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchProducts = async () => {
+	const res = await axios.get(
+		`https://bad-api-assignment.reaktor.com/products/jackets`
+	)
+	return res.data
+}
 
 const App = () => {
 	const [product, setProduct] = useState('jackets')
+
+	const { isLoading, isError, data, error } = useQuery(
+		'products',
+		fetchProducts
+	)
+
+	console.log('data', data)
+
+	{
+		isLoading && <div>Loading...</div>
+	}
+	{
+		isError && <div>Error: {error.message}</div>
+	}
 	return (
 		<div className='App'>
 			<h2>WAREHOUSE SPA</h2>
